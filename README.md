04cb38384f3c54142c6ac3791e9aefce3eb29ea9
ArisaGrimoire <arisa.yuugiri@gmail.com>
2025-09-04 00:35:42 +0900

    Initial commit with GUI PATH Editor prototype

---
+++ README.md
@@ -0,0 +1,3 @@
+# Windows Path Editor
+
+A simple GUI tool to edit Windows PATH variables.
---
+++ main.py
@@ -0,0 +1,5 @@
+from ui.path_editor_ui import PathEditorApp
+
+if __name__ == "__main__":
+    app = PathEditorApp()
+    app.mainloop()
